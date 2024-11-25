525412fd2433aa1e24cb278c1d0be6fc87167ef5
daniela <dggtn97@gmail.com>
2024-11-25 14:56:18 -0300

    realizo comunicacion entre cliente (Insomnia) y el API (backend) para recibir body (registro de medicos)

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,7 @@
 package med.voll.api.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void registrarMedico(){
+    public void registrarMedico(@RequestBody String parametro){
         System.out.println("El request llega correctamente");
+        System.out.println(parametro);
 
     }
 }
