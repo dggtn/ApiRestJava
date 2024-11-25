7fc48e8a26b3175e45bbc800c4f15031509eb047
daniela <dggtn97@gmail.com>
2024-11-25 15:14:01 -0300

    hacemos que request llegue a insomnia/cliente

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void registrarMedico(@RequestBody String parametro){
-        System.out.println("El request llega correctamente");
-        System.out.println(parametro);
+    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
+        System.out.println(datosRegistroMedico);
 
     }
 }
