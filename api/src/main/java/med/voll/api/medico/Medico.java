5e69cbaab70823ee5d23f797be391d6767e59450
daniela <dggtn97@gmail.com>
2024-12-02 09:01:50 -0300

    agrego clase Medico para hacer persistencia de datos para vincular con API

---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,11 @@
+package med.voll.api.medico;
+
+import med.voll.api.direccion.DatosDireccion;
+
+public class Medico {
+    String nombre;
+    String email;
+    String documento;
+    Especialidad especialidad;
+    DatosDireccion direccion;
+}
