25f497289fee11d87c37a3ef96b066e2009e1b08
daniela <dggtn97@gmail.com>
2024-12-02 09:22:38 -0300

    creo constructores y getters y setters para las clases Medico y Direccion

--- api/src/main/java/med/voll/api/direccion/Direccion.java
+++ api/src/main/java/med/voll/api/direccion/Direccion.java
@@ -11,4 +11,52 @@ public class Direccion {
     String numero;
     String complemento;
 
+
+    public Direccion(String calle, String distrito, String ciudad, String numero, String complemento) {
+        this.calle = calle;
+        this.distrito = distrito;
+        this.ciudad = ciudad;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getDistrito() {
+        return distrito;
+    }
+
+    public void setDistrito(String distrito) {
+        this.distrito = distrito;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -17,4 +17,61 @@ public class Medico {
     private Especialidad especialidad;
     @Embedded
     private Direccion direccion;
+
+    public Medico(Long id, String nombre, String email, String documento, Especialidad especialidad, Direccion direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.documento = documento;
+        this.especialidad = especialidad;
+        this.direccion = direccion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Direccion getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direccion = direccion;
+    }
 }
