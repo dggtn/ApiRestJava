42977eb119ce8f7e82301de8431351c5eaf31c42
daniela <dggtn97@gmail.com>
2024-12-02 18:55:48 -0300

    agrego request Mapping a medicoController

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,8 @@
 package med.voll.api.controller;
 
 import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
         System.out.println(datosRegistroMedico);
