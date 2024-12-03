ad98f68049d42dde37c220fe8f0b7f88e44781aa
daniela <dggtn97@gmail.com>
2024-12-03 18:37:11 -0300

    agrego validaciones @Valid y @NotNull a los parametros especialidad y direccion

--- api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,5 +1,6 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -15,8 +16,9 @@ public record DatosRegistroMedico(
         @NotBlank
         @Pattern(regexp ="\\d{4,6}")
         String documento,
-        @NotBlank
+        @NotNull
         Especialidad especialidad,
         @NotNull
+        @Valid
         DatosDireccion direccion) {
 }
