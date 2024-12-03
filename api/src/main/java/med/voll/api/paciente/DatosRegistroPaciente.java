d3338c750d3240fd41eb90d00d9a0c4323b19a55
daniela <dggtn97@gmail.com>
2024-12-03 18:17:19 -0300

    agrego validation constraints para validar que nombre no sea vacio, que cumpla con formato email y que los demas parametros no esten vacios

--- api/src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
+++ api/src/main/java/med/voll/api/paciente/DatosRegistroPaciente.java
@@ -1,4 +1,23 @@
 package med.voll.api.paciente;
 
-public class DatosRegistroPaciente {
+import med.voll.api.controller.Valid;
+import med.voll.api.direccion.DatosDireccion;
+import org.antlr.v4.runtime.misc.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+
+public record DatosRegistroPaciente(
+        @NotBlank
+        String nombre,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefono,
+        @NotBlank
+        String documentoIdentidad,
+        @NotNull @Valid
+        DatosDireccion direccion
+){
+
 }
