bdae1e219e3adaca193ff7d40a744b3fead74ad7
daniela <dggtn97@gmail.com>
2024-12-04 10:33:48 -0300

    agrego a la interfaz la opcion de devolver Optional<Object> findAll(Pageable paginacion)

--- api/src/main/java/med/voll/api/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -2,5 +2,9 @@ package med.voll.api.medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.Optional;
+
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Optional<Object> findAll(Pageable paginacion);
 }
