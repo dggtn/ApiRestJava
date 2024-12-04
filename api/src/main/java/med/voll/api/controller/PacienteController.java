f4a334d93537a2e8408a6e6d7a32016f0077bdab
daniela <dggtn97@gmail.com>
2024-12-04 10:32:02 -0300

    agrego paginacion al metodo listar por default

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,24 +2,31 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.paciente.DatosListaPaciente;
 import med.voll.api.paciente.DatosRegistroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes")
 public class PacienteController {
+
     @Autowired
     private PacienteRepository repository;
 
+    @GetMapping
+    public Page<DatosListaPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nombre"}) Pageable paginacion) {
+        return repository.findAll(paginacion).map(DatosListaPaciente::new);
+    }
     @PostMapping
     @Transactional
     public void registrar(@RequestBody @Valid DatosRegistroPaciente datos) {
         repository.save(new Paciente(datos));
     }
+
 }
