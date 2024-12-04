59b642ebd16c4791ce24d9f718193baeeebf2aeb
daniela <dggtn97@gmail.com>
2024-12-04 19:39:40 -0300

    agrego para metros para el record DatosActualizarMedico

--- api/src/main/java/med/voll/api/medico/DatosActualizarMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosActualizarMedico.java
@@ -1,7 +1,8 @@
 package med.voll.api.medico;
 
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.controller.Valid;
 import med.voll.api.direccion.DatosDireccion;
 
-public record DatosActualizarMedico(@NotNull Long id, String nombre, String documento, DatosDireccion direccion) {
+public record DatosActualizarMedico(@NotNull Long id, String nombre, String documento, String telefono, @Valid DatosDireccion direccion) {
 }
