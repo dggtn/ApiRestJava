fc0f42a7d04269653de8261a689e27a7eb04ca3b
daniela <dggtn97@gmail.com>
2024-12-04 10:31:02 -0300

    agrego paginacion al metodo listadoMedicos

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,9 @@ import med.voll.api.medico.DatosRegistroMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,8 +26,8 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DatosListadoMedico> listarMedicos(){
-        return medicoRepository.findAll().stream().map(DatosListadoMedico::new).toList();
+    public Page<DatosListadoMedico> listadoMedicos(@PageableDefault(size = 2) Pageable paginacion) {
+        return medicoRepository.findAll(paginacion).map(DatosListadoMedico::new);
     }
 
 }
