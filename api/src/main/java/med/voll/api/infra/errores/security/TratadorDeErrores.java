9525cdd0e97e2fa8a95e697166e47051c8b9d2f7
daniela <dggtn97@gmail.com>
2024-12-05 14:39:42 -0300

    agrego variable errores para mapear en una lista y creo record llamado DatosErrorValidacion

--- api/src/main/java/med/voll/api/infra/errores/security/TratadorDeErrores.java
+++ api/src/main/java/med/voll/api/infra/errores/security/TratadorDeErrores.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.errores.security;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -17,7 +18,13 @@ public class TratadorDeErrores {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarError400(MethodArgumentNotValidException e){
-        var errores = e.getFieldError();
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
         return ResponseEntity.badRequest().body(errores);
+    }
+    private record DatosErrorValidacion(String campo,String error){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(),error.getDefaultMessage());
+        }
+
     }
 }
