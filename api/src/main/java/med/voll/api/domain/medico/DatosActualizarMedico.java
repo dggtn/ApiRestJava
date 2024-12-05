2d4ad0df98a97a80fc777597bd979f08afd984ce
daniela <dggtn97@gmail.com>
2024-12-05 11:56:30 -0300

    agrego @Valid para el parametro direccion de tipo DatosDireccion

--- api/src/main/java/med/voll/api/domain/medico/DatosActualizarMedico.java
+++ api/src/main/java/med/voll/api/domain/medico/DatosActualizarMedico.java
@@ -1,8 +1,8 @@
-package med.voll.api.medico;
+package med.voll.api.domain.medico;
 
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.controller.Valid;
-import med.voll.api.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.DatosDireccion;
 
 public record DatosActualizarMedico(@NotNull Long id, String nombre, String documento, String telefono, @Valid DatosDireccion direccion) {
 }
