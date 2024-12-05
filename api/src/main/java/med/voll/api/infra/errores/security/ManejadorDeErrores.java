fbb6b0c8dd97ccb8634ff992d10cf610be381686
daniela <dggtn97@gmail.com>
2024-12-05 17:54:16 -0300

    paso clae ManejadorErrores al paquete errors

--- api/src/main/java/med/voll/api/infra/errores/security/ManejadorDeErrores.java
+++ api/src/main/java/med/voll/api/infra/errores/security/ManejadorDeErrores.java
@@ -1,5 +1,4 @@
-package med.voll.api.infra.errores.security;
-
+package med.voll.api.controller;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
@@ -9,22 +8,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class TratadorDeErrores {
+public class ManejadorDeErrores {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarError404(){
+    public ResponseEntity manejarError404() {
         return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
-        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
-        return ResponseEntity.badRequest().body(errores);
+    public ResponseEntity manejarErro400(MethodArgumentNotValidException ex) {
+        var errores = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errores.stream().map(DatosErrorValidacion::new).toList());
     }
-    private record DatosErrorValidacion(String campo,String error){
-        public DatosErrorValidacion(FieldError error){
-            this(error.getField(),error.getDefaultMessage());
-        }
 
+    private record DatosErrorValidacion(String campo, String mensaje) {
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
