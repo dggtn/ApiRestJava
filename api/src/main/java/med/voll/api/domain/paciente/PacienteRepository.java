c4ce7c80d50c74b84700083a0a7cd4e976764b05
daniela <dggtn97@gmail.com>
2024-12-05 12:07:18 -0300

    agrego Page<Paciente> findAllByActivoTrue(Pageable paginacion)

--- api/src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ api/src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,10 +1,10 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
-    Optional<Object> findAllByActivoTrue(Pageable paginacion);
+    Page<Paciente> findAllByActivoTrue(Pageable paginacion);
 }
