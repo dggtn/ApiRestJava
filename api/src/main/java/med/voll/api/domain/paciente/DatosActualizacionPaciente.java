be5b659ff142807fb4773244a26941d82d58781e
daniela <dggtn97@gmail.com>
2024-12-05 12:04:37 -0300

    agrego validaciones @NotNull y @Valid para el id y la direccion

--- api/src/main/java/med/voll/api/domain/paciente/DatosActualizacionPaciente.java
+++ api/src/main/java/med/voll/api/domain/paciente/DatosActualizacionPaciente.java
@@ -1,8 +1,8 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.paciente;
 
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.controller.Valid;
-import med.voll.api.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.DatosDireccion;
 
 public record DatosActualizacionPaciente(@NotNull Long id, String nombre, String documento, String telefono, @Valid DatosDireccion direccion){
 }
