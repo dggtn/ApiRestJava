5f15ed853c58f55241178747c2f96c646c6cdd59
daniela <dggtn97@gmail.com>
2024-12-16 09:46:32 -0300

    agrego atributos,tabla y entidad a la clase Usuario

--- api/src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ api/src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -1,4 +1,22 @@
 package med.voll.api.domain.usuario;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuario")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
 public class Usuario {
+    private Long id;
+    private String login;
+    private String clave;
+
 }
