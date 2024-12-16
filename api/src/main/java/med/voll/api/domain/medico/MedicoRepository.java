36e5db3cdbf1a9bdc9da9df69378d357936d4061
daniela <dggtn97@gmail.com>
2024-12-16 09:44:22 -0300

    creo clase Usuario

--- api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,12 +1,14 @@
 package med.voll.api.domain.medico;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
-    Optional<Object> findAll(Pageable paginacion);
+    List<Medico> findAll(Sort s);
     Page<Medico> findByActivoTrue(org.springframework.data.domain.Pageable paginacion);
 }
