684f9f0a58c3ebcda4b7be6c4886ae14ca2da769
daniela <dggtn97@gmail.com>
2024-12-22 15:06:59 -0300

    creo restController advce

--- api/src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
+++ api/src/main/java/med/voll/api/infra/errores/TratadorDeErrores.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ManejadorDeErrores {
+public class TratadorDeErrores {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity manejarError404() {
