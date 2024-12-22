53a4e6e743056e354077523f2d970e1f22d857fb
daniela <dggtn97@gmail.com>
2024-12-22 15:05:42 -0300

    Creo interfaz para Usuario

--- api/src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ api/src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -1,7 +1,9 @@
 package med.voll.api.domain.usuario;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
 
+   static UserDetails findByUsername(String username);
 }
