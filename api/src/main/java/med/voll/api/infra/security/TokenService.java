54ede5c4cff1930257fb814b35a19cfbf6890b7a
daniela <dggtn97@gmail.com>
2024-12-23 12:30:27 -0300

    agrego try y catch para crear jwt con algoritmo HMAC256 ya que no tenemos privatekey

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,10 +1,23 @@
 package med.voll.api.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.stereotype.Service;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Service
 public class TokenService {
     public String generarToken(){
-        return null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("123456");
+            String token = JWT.create()
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            // Invalid Signing configuration / Couldn't convert Claims.
+        }
     }
 }
