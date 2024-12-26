af2a77c95595f27ad1f42ea3ab6203a6dc73d40c
daniela <dggtn97@gmail.com>
2024-12-26 15:53:31 -0300

    creo clase SecurityFilter que hereda de OncePerRequestFilter

--- api/src/main/java/med/voll/api/infra/security/DatosJWToken.java
+++ api/src/main/java/med/voll/api/infra/security/DatosJWToken.java
@@ -1,4 +1,4 @@
 package med.voll.api.infra.security;
 
-public record DatosJWToken(String jwToken) {
+public record DatosJWToken(String JWToken) {
 }
---
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,20 @@
+package med.voll.api.infra.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.xml.crypto.dsig.spec.XPathType;
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("Filtro esta siendo llamado");
+    }
+}
