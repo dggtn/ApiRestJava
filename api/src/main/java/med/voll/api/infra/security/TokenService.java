ed5b2a120a8e572e185c5f30b78fb4f71b8402ea
daniela <dggtn97@gmail.com>
2024-12-26 11:37:14 -0300

    agrego metodo generarFechaExpiracion() como parametro del metodo withExpiresAt() para crear el jwt

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,22 +3,33 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
 import org.springframework.stereotype.Service;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-    public String generarToken(){
+    public String generarToken(Usuario usuario){
         try {
             Algorithm algorithm = Algorithm.HMAC256("123456");
             return JWT.create()
                     .withIssuer("voll med")
-                    .withSubject("diego.rojas")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id",usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException();
         }
+    }
+    private Instant generarFechaExpiracion(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
+
     }
 }
