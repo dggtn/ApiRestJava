dcca1fa77b9848a48800584ab763eb1745e1e6df
daniela <dggtn97@gmail.com>
2024-12-27 12:19:34 -0300

    agrego función de Spring Security conocida como Method Security para restringir acceso a ciertas funciones y en SecurityConfigurations agrego @EnableMethodSecurity para permitirlo

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -75,6 +76,13 @@ public class MedicoController {
         Medico medico = medicoRepository.getReferenceById(id);
         medico.desactivarMedico();
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    @Secured("ROLE_ADMIN")
+    public ResponseEntity detallar(@PathVariable Long id) {
+        var medico = medicoRepository.getReferenceById(id);
+        return ResponseEntity.ok(new DatosListadoMedico(medico));
     }
 
 }
