6717cead7eb2a99a1be924258781ad53c5c54297
daniela <dggtn97@gmail.com>
2024-12-26 23:00:26 -0300

    creo clase SecurityFilter para obtener el token del header

--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -16,6 +16,7 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         System.out.println("Filtro esta siendo llamado");
+        //Obterner el Token del header
         filterChain.doFilter(request,response);
     }
 }
