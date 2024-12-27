f11f2cc27d6508089ce50f90c7af1ea80b84de2d
daniela <dggtn97@gmail.com>
2024-12-27 11:14:27 -0300

    creo el constructor de datosListaPaciente

--- api/src/main/java/med/voll/api/domain/paciente/DatosListaPaciente.java
+++ api/src/main/java/med/voll/api/domain/paciente/DatosListaPaciente.java
@@ -2,9 +2,9 @@
 package med.voll.api.domain.paciente;
 
 
-public record DatosListaPaciente(Long id, String nombre, String email, String documento) {
 
+public record DatosListaPaciente(String nombre, String email, String documentoIdentidad) {
     public DatosListaPaciente(Paciente paciente) {
-        this(paciente.getId(), paciente.getNombre(), paciente.getEmail(), paciente.getDocumento());
+        this(paciente.getNombre(), paciente.getEmail(), paciente.getDocumentoIdentidad());
     }
 }
